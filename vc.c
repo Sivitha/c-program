24cbc457a53aace829115ea236ce52f481afff89
Sivitha <sivithag@gmail.com>
2017-06-21 15:45:01 +0530

    Create vc.c

---
+++ vc.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char n;
+printf("enter the value of n");
+scanf("%c",&n);
+if(n==a||n==e||n==i||n==o||n==u)
+printf("the letter is vovel");
+else 
+printf("the letter is consonent");
+}
